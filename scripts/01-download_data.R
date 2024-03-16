e45bf64d294b75c4cf7368851075fa8e00310cd9
Mohammed Yusuf Shaikh <yusuf.mns@icloud.com>
2024-03-16 16:15:57 +0000

    Data Loaded and cleaned

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,37 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Download the data
+# Author: Mohammed Yusuf Shaikh
+# Date: 7th March 2024
+# Contact: mohammedyusuf.shaikh@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Install Packages
+
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+install.packages("readr")
+install.packages("tidyverse")
+install.packages("janitor")
+library(readr)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+
+url <- "https://data.leicester.gov.uk/api/explore/v2.1/catalog/datasets/covid-19-positive-tests-and-rate-per-100000-population-for-leicester-and-englan0/exports/csv"
+
+# Read the CSV file using read_csv from the readr package
+the_raw_datacovid_19_positive_tests_and_rate_per_100000_population_for_leicester_and_england <- read_csv(url)
+
+# Print the first few lines of the data frame to check
+head(the_raw_datacovid_19_positive_tests_and_rate_per_100000_population_for_leicester_and_england)
+
 
 
 
 #### Save data ####
-# [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(the_raw_datacovid_19_positive_tests_and_rate_per_100000_population_for_leicester_and_england, "data/raw_data/raw_data.csv") 
 
          
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,58 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Clean the data
+# Author: Mohammed Yusuf Shaikh
+# Date: 7th March 2024
+# Contact: mohammedyusuf.shaikh@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Install Packages
+
 
 #### Workspace setup ####
+install.packages("tidyverse")
+install.packages("readr")
+install.packages("dplyr")
+install.packages("janitor")
+install.packages("lubridate")
 library(tidyverse)
-
+library(readr)
+library(dplyr)
+library(lubridate)
+library(janitor)
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
+# Reading the data with specified delimiter using read_delim separate columns
+raw_data <- read_delim("data/raw_data/raw_data.csv", delim = ";")
+
+# Checking the first few rows to ensure it read correctly
+head(raw_data)
+
+
+clean_data <- clean_data |>
+  drop_na()
+
+
+clean_data <- raw_data |>
+  rename(
+    AreaName = areaname,
+    Date = date,
+    NewCasesBySpecimenDate = newcasesbyspecimendate,
+    NewCasesRatePer100k = newcasesbyspecimendaterollingrate,
+    NewCasesRollingSum = newcasesbyspecimendaterollingsum
+  )
+
+head(clean_data)
+names(clean_data)
+head(clean_data$Date)
+
+# Adding separate column for year, month, date
+
+clean_data <- clean_data |>
   mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+    year = year(Date),  # Extract the year from the date column
+    month = month(Date),
+    day = day(Date)     # Extract the day from the date column
+  )
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(clean_data, "data/analysis_data/analysis_data.csv")
